4977ee03d9219c75eefb2da740195137933bf861
Alexander <romian37@gmail.com>
2026-04-15 13:09:13 +0700

    Variables and Data Types

--- fundamentals/Variables And Data Types/index.js
+++ fundamentals/Variables And Data Types/index.js
@@ -4,3 +4,30 @@ var name = 'Alex'
 
 console.log(name)
 console.log('1' + true);
+
+var warning = 'test'
+
+// alert(warning)
+//
+// var age = prompt('How old are you')
+// console.log('The user is ' + age + ' years old')
+
+// Basic Operators
+var totalDistance, alexDistance, samDistance;
+totalDistance = 20;
+
+alexDistance = totalDistance - 8;
+samDistance = totalDistance - 12;
+console.log('Alex distance is: ' + alexDistance)
+console.log('Sam distance is: ' + samDistance)
+console.log(alexDistance > samDistance)
+
+// Boolean Logic
+var x = 10;
+// javascript... always use triple equals
+console.log(x == '10')
+console.log(x === '10')
+console.log(x !== '10')
+
+// Logical Operators
+console.log(x > 10 && x >= 10)
